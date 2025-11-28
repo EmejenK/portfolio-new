3eaa5cc55c04af5993613e7d85c1e0a75aad5096
Emejen <emejenmwovio@gmail.com>
2025-11-28 22:10:31 +0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function myFuction() {
-    document.getElementById("navigation").style.marginRight = "0";
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100px";
+  document.getElementById("main").style.marginRight = "0";
 }
 
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
 
 
 
